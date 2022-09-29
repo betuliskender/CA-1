476cba7e30bb6fdd80c41165665be27af6b99c81
Betül Iskender <betul.iskender@outlook.dk>
2022-09-29 13:14:26 +0200

    hent api kode fået fra Thomas og sat ind skal stadig testes

--- src/main/java/facades/ZipFacade.java
+++ src/main/java/facades/ZipFacade.java
@@ -1,10 +1,23 @@
 package facades;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.ZipDto;
+import entities.Hobby;
+import org.eclipse.persistence.sessions.serializers.JSONSerializer;
+
+import javax.json.JsonArray;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
 
 public class ZipFacade {
 
@@ -17,16 +30,30 @@ public class ZipFacade {
 //        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 //        System.out.println(response.body());
 
-        URL url = new URL("https://api.dataforsyningen.dk/postnumre");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }in.close();
-        System.out.println(content);
+//        URL url = new URL("https://api.dataforsyningen.dk/postnumre");
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//        con.setRequestMethod("GET");
+//        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//        String inputLine;
+//        StringBuffer content = new StringBuffer();
+//        while ((inputLine = in.readLine()) != null) {
+//            content.append(inputLine);
+//        }in.close();
+//        //System.out.println(content);
 
+        Gson GSON = new GsonBuilder().create();
+        String urlString = "https://api.dataforsyningen.dk/postnumre";
+        URL yahoo = new URL(urlString);
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader( new InputStreamReader( yc.getInputStream()));
+        String inputLine;
+        StringBuilder sb = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null)
+            sb.append(inputLine);
+        in.close();
+        System.out.println(sb);
+        ZipDto[] myDtos = GSON.fromJson(sb.toString(), ZipDto[].class);
+        System.out.println(myDtos.length);
     }
 }
