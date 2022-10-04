e09d12c7ffd6b3996ba0e2139355e182bf43fdd0
Denis <hvorerden@hotmail.com>
2022-10-04 09:12:34 +0200

    started on personhandler

--- src/main/java/services/PersonHandler.java
+++ src/main/java/services/PersonHandler.java
@@ -1,4 +1,38 @@
 package services;
 
+import dtos.AddressDto;
+import dtos.CityInfoDto;
+import dtos.PersonDto;
+import entities.Address;
+import entities.CityInfo;
+import entities.Person;
+
 public class PersonHandler {
+    public static Person mergeDTOAndEntity(PersonDto personDto, Person person)
+    {
+        Person updatedPerson = person;
+
+        if(personDto.getEmail() != null && !personDto.getEmail().equals(person.getEmail()))
+        {
+            updatedPerson.setEmail(personDto.getEmail());
+        }
+
+        if(personDto.getFirstName() != null && !personDto.getFirstName().equals(person.getFirstName()))
+        {
+            updatedPerson.setFirstName(personDto.getFirstName());
+        }
+        if(personDto.getLastName() != null && !personDto.getLastName().equals(person.getLastName()))
+        {
+            updatedPerson.setLastName(personDto.getLastName());
+        }
+        if(personDto.getAddress() != null && !personDto.getAddress().equals(person.getAddress()))
+        {
+            updatedPerson.setAddress(new Address(personDto.getAddress().getStreet(), personDto.getAddress().getAdditionalInfo(), new CityInfo());
+
+this.cityInfo = new CityInfo(addressDto.getInnerCityInfoDto().getId(),addressDto.getInnerCityInfoDto().getZipcode(),addressDto.getInnerCityInfoDto().getCity());
+
+        }
+
+        return updatedPerson;
+    }
 }
