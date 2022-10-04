64cb6468c145ba2855dfe96c13adbc67238135c3
Betül Iskender <betul.iskender@outlook.dk>
2022-10-04 10:58:45 +0200

    fixed more

--- src/test/java/facades/PersonFacadeTest.java
+++ src/test/java/facades/PersonFacadeTest.java
@@ -106,17 +106,10 @@ public class PersonFacadeTest {
         PersonDto personDto = new PersonDto(newPerson);
         PersonDto actual = facade.create(personDto);
         assertNotNull(actual.getId());
-<<<<<<< HEAD
         System.out.println(actual);
         System.out.println(actual.getHobbies());
     }
-=======
-        System.out.println(newPerson);
-        System.out.println(newPerson.getHobbies());
     }
-
->>>>>>> 12e9d8f (person id er null)
-
 
 //
 //    @Test
@@ -150,5 +143,3 @@ public class PersonFacadeTest {
 //
 
 
-
-}
