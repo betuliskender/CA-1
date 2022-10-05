077da65aacddf9ea888b0f6aa721b58bff835b1d
Denis <hvorerden@hotmail.com>
2022-10-05 12:00:48 +0200

    cityinforesourcetest

--- src/test/java/rest/CityInfoResourceTest.java
+++ src/test/java/rest/CityInfoResourceTest.java
@@ -1,10 +1,19 @@
 package rest;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.AddressDto;
+import dtos.CityInfoDto;
+import dtos.HobbyDto;
 import entities.Address;
 import entities.CityInfo;
+import entities.Hobby;
 import entities.RenameMe;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -19,9 +28,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
 //Uncomment the line below, to temporarily disable this test
 //@Disabled
 
@@ -29,6 +41,7 @@ public class CityInfoResourceTest {
 
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static CityInfo c1, c2;
 
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
@@ -61,6 +74,7 @@ public class CityInfoResourceTest {
         EMF_Creator.endREST_TestWithDB();
         httpServer.shutdownNow();
     }
+
     // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
     //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
     @BeforeEach
@@ -79,24 +93,90 @@ public class CityInfoResourceTest {
         }
     }
 
-
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
-        given().when().get("/address").then().statusCode(200);
+        given().when().get("/cityinfo").then().statusCode(200);
     }
     @Test
     public void testLogRequest() {
         System.out.println("Testing logging request details");
         given().log().all()
-                .when().get("/address")
+                .when().get("/cityinfo")
                 .then().statusCode(200);
     }
     @Test
     public void testLogResponse() {
         System.out.println("Testing logging response details");
         given()
-                .when().get("/cityInfo")
+                .when().get("/cityinfo")
                 .then().log().body().statusCode(200);
+    }
+    @Test
+    public void testGetById()  {
+        given()
+                .contentType(ContentType.JSON)
+//                .pathParam("id", p1.getId()).when()
+                .get("/cityinfo/{id}",c1.getId())
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("id", equalTo(c1.getId()))
+                .body("zipcode", equalTo(c1.getZipcode()))
+                .body("city", equalTo(c1.getCity()));
+    }
+    @Test
+    public void getAll() throws Exception {
+        List<CityInfoDto> cityInfoDtos;
+
+        cityInfoDtos = given()
+                .contentType("application/json")
+                .when()
+                .get("/cityinfo")
+                .then()
+                .extract().body().jsonPath().getList("", CityInfoDto.class);
+
+        CityInfoDto c1Dto = new CityInfoDto(c1);
+        CityInfoDto c2Dto = new CityInfoDto(c2);
+        assertThat(cityInfoDtos, containsInAnyOrder(c1Dto, c2Dto));
+
+    }
+    @Test
+    public void create() {
+        CityInfo c = new CityInfo("8800", "Tarm");
+        CityInfoDto cityInfoDto = new CityInfoDto(c);
+        String requestBody = GSON.toJson(cityInfoDto);
+        System.out.println(cityInfoDto);
+
+        given()
+                .header("Content-type", ContentType.JSON)
+                .and()
+                .body(requestBody)
+                .when()
+                .post("/cityinfo")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("id", notNullValue())
+                .body("zipcode", equalTo(cityInfoDto.getZipcode()))
+                .body("city", equalTo(cityInfoDto.getCity()));
+    }
+    @Test
+    public void updateTest() {
+        c1.setZipcode("9001");
+        CityInfoDto cityInfoDto = new CityInfoDto(c1);
+        String requestBody = GSON.toJson(cityInfoDto);
+
+        given()
+                .header("Content-type", ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .put("/cityinfo/"+c1.getId())
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("id", equalTo(c1.getId()))
+                .body("zipcode", equalTo("9001"))
+                .body("city", equalTo(cityInfoDto.getCity()));
     }
 }
